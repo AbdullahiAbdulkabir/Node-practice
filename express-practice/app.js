6014cb465a818cc6359ba57b8783692df0c1a3e9
Abdullahi Abdulkabir <abdullahiabdulkabir1@gmail.com>
2022-03-01 18:51:38 +0100

    :rocket: Book library practice

--- express-practice/app.js
+++ express-practice/app.js
@@ -2,8 +2,24 @@ const express = require('express')
 
 const app = express();
 
+app.set('view engine', 'ejs')
 app.listen(4000)
 
+
 app.get('/',(req, res)=> {
-    res.send( "<h1> Hello world in Express Js </h1>")
+    res.render('index',{title: "Home Page in ejs"})
+    // res.sendFile('./views/index.html',{root: __dirname})
+    // res.sendFile('./views/index.html',{root: __dirname})
+    // res.send( "<h1> Hello world in Express Js </h1>")
+})
+
+
+app.get('/about',(req, res)=> {
+    res.sendFile('./views/index.html',{root: __dirname})
+    // res.send( "<h1> Hello world in Express Js </h1>")
+})
+
+app.use((req, res)=>{
+    res.sendFile('./views/404.html',{root: __dirname})
+
 })
